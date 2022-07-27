5a0fbff2c51ee20b7d96d841839927502e4fd97a
Eva Clark <101047194+evaclark@users.noreply.github.com>
2022-07-27 12:17:12 +0930

    Update server.c

--- server.c
+++ server.c
@@ -12,6 +12,17 @@
 
 #include "minitalk.h"
 
+/*this function will first evaluate whether the first signal of the character
+ * either a 1 or 0, using the & bitwise operator to return a 1 if true, 0 if 
+ * false.
+ * this value will then be shifted to the left according to i, which will
+ * increase in value every time - eg.
+ * 65 = A, in binary this is 01000001 - we only send 7 digits through client, 1000001
+ * 0 += (1 & 1) << 0 = 1
+ * 1 += (1 & 0) << 1 = 1 += (0 << 1 = 0) = 1 += 0 = 1
+ * 1 += (0 & 1) << 2 = 1 += (0 << 2) = 1 += 0 = 1
+ * …
+ * 1 += (1 & 1) << 6 = 1 += (1 << 6) = 1 += 1000000 = 01000001 = A */
 void	decoder(int signal)
 {
 	g_msg.c += ((signal & 1) << g_msg.i);
@@ -25,6 +36,10 @@ void	decoder(int signal)
 	}
 }
 
+/*the struct is made into a global variable so we can initialise the values of
+ * i and c in the main function, as if we do it in decoder the values will
+ * reset everytime a signal is recieved instead of at the end of every
+ * character */
 int	main(void)
 {
 	g_msg.i = 0;
